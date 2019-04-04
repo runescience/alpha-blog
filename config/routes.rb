09809ad435d8abe761da53f250e5a4ab6b4a4345
joel siragher <joel.siragher@gmail.com>
2019-04-04 22:03:56 +0000

    create rails app and setup home and about pages

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#JDSJDS Was: get 'pages/home', to: 'pages#home'
+root 'pages#home'
+
+#JDSJDS Was: get 'pages/about', to: 'pages#about'
+get 'about', to: 'pages#about'
+
+end
