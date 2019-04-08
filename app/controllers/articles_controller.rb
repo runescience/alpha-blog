a560aa34e40ff4b895b1611674c1ae0ec6fea149
joel siragher <joel.siragher@gmail.com>
2019-04-08 19:22:26 +0000

    fix create and addflash and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,16 +5,37 @@ class ArticlesController < ApplicationController
 
   def create
     
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+
+    if @article.save
+      #do something
+      flash[:notice] = "article was successfully created"
+      redirect_to article_path(@article)
+    else
+      # problems
+      
+      render 'new'
+    end
+  
+   # @article.save
+   # redirect_to articles_path(@article)
   end
+
+  def show
+    
+    @article = Article.find(params[:id])
+    
+    
+    
+  end
+
 
   private 
   def article_params
     params.require(:article).permit(:title, :description)
   end
+
+
 
 end
 
